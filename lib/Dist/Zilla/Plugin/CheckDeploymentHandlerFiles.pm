16dde60ee3d1e27f6ceb0c544a76329c624d1ff6
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-08-10 11:06:26 +0100

    initial commit

---
+++ lib/Dist/Zilla/Plugin/CheckDeploymentHandlerFiles.pm
@@ -0,0 +1,35 @@
+package Dist::Zilla::Plugin::CheckDeploymentHandlerFiles;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Dist::Zilla::Plugin::CheckDeploymentHandlerFiles - Module abstract
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Report any issues on the public bugtracker.
+
+=head1 AUTHOR
+
+Alastair McGowan-Douglas <altreus@altre.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2016 by Alastair McGowan-Douglas.
+
+This is free software, licensed under:
+
+  The (three-clause) BSD License
+
+=head1 SEE ALSO
+
